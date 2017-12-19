1b967e3307b3ce761be1a1e93ff4a1a40042ee68
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2017-12-19 03:22:59 -0800

    common, header, footer, section, banner layout upload

--- common.js
+++ common.js
@@ -1,2 +1,40 @@
 // script handling events that are common on multiple pages
 // such as header, footer behaviors.
+$(function(){
+
+  $('menubar').on('click', function() {
+    $('body').toggleClass("scrollDisabled");
+    $('#menuList').slideToggle();
+    $('.menubarIcon').toggleClass("change");
+    $('.menu-overlay').toggle();
+  });
+
+  $(window).on('scroll', function() {
+    if ($(window).scrollTop() >= 20) {
+      $('#header-logo').css({"height": "60px"});
+      $('#menuList').css({"top": "68px"});
+      $('#whitespace-header').css({"height": "70px"});
+    } else {
+      $('#header-logo').css({"height": "100px"});
+      $('#menuList').css({"top": "108px"});
+      $('#whitespace-header').css({"height":"110px"});
+    }
+  });
+
+});
+
+$(window).on('load', function() {
+  // display banner, section, footer, and logo image after all images are loaded
+
+  $('section').animate({
+    opacity: 1
+  }, 700, function() {
+    $('#header-logo').css({"opacity": 1, "height": "100px"});
+    $('#whitespace-header').css({"height": "110px"});
+  })
+  $('footer').css({"opacity": 1});
+
+  $('#small-banner').animate({
+    opacity: 1
+  }, 1000);
+})
--- home.js
+++ home.js
@@ -1,1 +1,30 @@
 // script controlling events in main homepage.
+$(function() {
+
+  $('a[href="/"]').on('click', function(e){
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 500);
+  });
+
+});
+
+$(window).on('load', function() {
+  // display banner, section, footer, and logo image after all images are loaded
+  $('#banner').animate({
+    opacity: 1
+  }, 800, function() {
+    $('#banner-text').css({"opacity":1});
+  });
+  $('section').animate({
+    opacity: 1
+  }, 700, function() {
+    if ($(window).scrollTop() >= 20) {
+      $('#header-logo').css({"opacity": 1, "height": "60px"});
+    } else {
+      $('#header-logo').css({"opacity": 1, "height": "100px"});
+    }
+  })
+  $('footer').css({"opacity": 1});
+})
