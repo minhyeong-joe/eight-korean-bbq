4c0d77c385017b6ff3d158806e8f87502e67de5a
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2017-12-20 00:14:48 -0800

    Home page layout and animation initial finished
    Former-commit-id: f44ff81aa2e8cc7f507e970d033d3b0e044f4b67

--- home.js
+++ home.js
@@ -1,12 +1,66 @@
 // script controlling events in main homepage.
 $(function() {
 
+  // home link-> instead of reloading, simply slide to top
   $('a[href="/"]').on('click', function(e){
     e.preventDefault();
     $('html,body').animate({
       scrollTop: 0
     }, 500);
   });
+
+  // 8 flavors details
+  var sliding = false;
+
+  $('.flavorBtn').on('click', function() {
+    var details = this.id + "-details";
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      sliding = true;
+      $('#'+details).slideUp(150, function() {
+        sliding = false;
+      });
+    } else if (!$('.flavorBtn').hasClass("active") && !sliding) {
+      $(this).addClass("active");
+      sliding = true;
+      $('#'+details).css({"display":"flex"}).hide().slideDown(300, function() {
+          sliding = false;
+        });
+    } else if (!sliding) {
+      $('.flavorBtn.active').removeClass("active");
+      $(this).addClass("active");
+      sliding = true;
+      $('#eightflavors-description>div:not(#'+details+')').slideUp(150, function() {
+        $('#'+details).css({"display":"flex"}).hide().slideDown(300, function() {
+          sliding = false;
+        })
+      });
+    }
+  });
+
+  // press videos (food-network, cooking channel)
+  $('.video-press').on('click', function(e) {
+    e.preventDefault();
+    var vid = this.id;
+    var video = document.getElementById("press-video");
+    $('#video-overlay source').attr("src", "vid/"+vid+'.mp4');
+    $('#press-video')[0].load();
+    $('#video-overlay').fadeIn(500);
+    $('body').addClass("scrollDisabled");
+    video.play();
+  });
+
+  $('#video-overlay').on('click', function(e) {
+    var video = document.getElementById("press-video");
+    if (e.target.id === 'close') {
+      video.pause();
+      $('#video-overlay source').removeAttr("src");
+      $('#video-overlay').fadeOut(300);
+      $('body').removeClass("scrollDisabled");
+    } else if (e.target.id === 'press-video') {
+      video.paused ? video.play() : video.pause();
+    }
+  })
 
 });
 
