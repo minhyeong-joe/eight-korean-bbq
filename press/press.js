d5cbebde2383918769740bc99d18fbfa24e5d02d
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2017-12-24 15:56:41 -0800

    press video user friendly

--- press/press.js
+++ press/press.js
@@ -2,20 +2,38 @@ $(function() {
 
   $('.video-press').on('click', function(e) {
     e.preventDefault();
-    var vid = this.id;
-    var video = document.getElementById("press-video");
-    $('#video-overlay source').attr("src", "https://s3-us-west-1.amazonaws.com/eight-korea-bbq-vid/"+vid+".mp4");
-    $('#press-video')[0].load();
-    $('#video-overlay').fadeIn(500);
-    $('body').addClass("scrollDisabled");
-    video.play();
+    var self = this;
+    $('#video-confirm-modal-overlay').css({"display":"flex"}).hide().fadeIn(300);
+    $('body').addClass('scrollDisabled');
+    $('#modal-continue').on('click', function() {
+      $('#video-confirm-modal-overlay').fadeOut(200);
+      var vid = self.id;
+      var video = document.getElementById("press-video");
+      video.src = "https://s3-us-west-1.amazonaws.com/eight-korea-bbq-vid/"+vid+".mp4";
+      // $('#video-overlay source').attr("src", "https://s3-us-west-1.amazonaws.com/eight-korea-bbq-vid/"+vid+".mp4");
+      video.load();
+      $('#video-overlay').fadeIn(500);
+      $('#press-video').on('canplay', function(){
+        $('#video-loading').fadeOut(100);
+        $('#press-video').fadeIn(100);
+        video.play();
+      })
+    });
+    $('#modal-cancel').on('click', function() {
+      $('#video-confirm-modal-overlay').fadeOut(200);
+      $('body').removeClass('scrollDisabled');
+    })
+
   });
 
   $('#video-overlay').on('click', function(e) {
     var video = document.getElementById("press-video");
     if (e.target.id === 'close') {
       video.pause();
-      $('#video-overlay source').removeAttr("src");
+      video.src = "";
+      video.load();
+      $('#press-video').fadeOut(100);
+      $('#video-loading').fadeIn(100);
       $('#video-overlay').fadeOut(300);
       $('body').removeClass("scrollDisabled");
     } else if (e.target.id === 'press-video') {
