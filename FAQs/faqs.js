426bc43530ec677ac38e3afe741bc3d7729f5253
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2017-12-24 12:19:17 -0800

    banner minor fix, some faqs added

--- FAQs/faqs.js
+++ FAQs/faqs.js
@@ -3,7 +3,7 @@ $(function() {
   function closeAccordion() {
     $('.accordion-title').removeClass('active');
     $('.accordion-content').slideUp(300).removeClass('open');
-  }
+  };
 
   $('.accordion-title').on('click', function(e) {
     e.preventDefault();
@@ -17,6 +17,17 @@ $(function() {
 
       $('.accordion-content' + current).slideDown(300).addClass('open');
     }
+  });
+
+  $('#parking-btn').on('click',function(e){
+    e.preventDefault();
+    $('#parking-overlay').fadeIn(300);
+    $('body').addClass("scrollDisabled");
+  });
+
+  $('#parking-overlay').on('click', function(){
+    $('#parking-overlay').fadeOut(300);
+    $('body').removeClass("scrollDisabled");
   })
 
 });
