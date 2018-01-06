d5ab01e114d2c425b5290bbc3ddd07e07e691fdc
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-01-05 19:37:04 -0800

    email php testing

--- franchise/franchise.php
+++ franchise/franchise.php
@@ -18,7 +18,7 @@ if (count($_POST)>0) {
   $mailheader .= "Reply-To: $email \r\n";
   $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
   $maiheader .="MIME-Version: 1.0\r\n";
-  $MESSAGE_BODY = "<html><head><link href='https://fonts.googleapis.com/css?family=Lato|Montserrat' rel='stylesheet'></head><body><br><br><br><h2 style='font-family: Lato, sans-serif;'>You have a new franchise inquiry from <a href='https://eightkoreanbbq.com/franchise'>Eight Korean BBQ</a></h2><hr style='width:700px; margin: 20px 0;'><br>";
+  $MESSAGE_BODY = "<html><head></head><body><br><br><br><h2 style='margin: auto;'>You have a new franchise inquiry from <a href='https://eightkoreanbbq.com/franchise'>Eight Korean BBQ</a></h2><hr style='width:700px; margin: 20px auto;'><br>";
   $MESSAGE_BODY .= "<table rules='all' style='border-color: #666; width: 650px; font-family: Montserrat, sans-serif; margin:auto;' cellpadding='10'>";
   $MESSAGE_BODY .= "<tr style='background: #eee;'><td style='width:120px; text-align:center;'><strong>Name:</strong></td><td>$first_name $last_name</td></tr>";
   $MESSAGE_BODY .= "<tr><td style='text-align:center;'><strong>Email:</strong></td><td><a href='mailto:$email'>$email</a></td></tr>";
@@ -27,7 +27,7 @@ if (count($_POST)>0) {
   $MESSAGE_BODY .= "<tr><td style='text-align:center;'><strong>Will you run day to day operations yourself?</strong></td><td>$runOperations</td></tr>";
   $MESSAGE_BODY .= "<tr><td style='text-align:center;'><strong>Cash available to invest: </strong></td><td>$cashInvest</td></tr>";
   $MESSAGE_BODY .= "<tr><td style='text-align:center;'><strong>Message:</strong></td><td>$message</td></tr></table><br><br>";
-  $MESSAGE_BODY .= "<img src='https://eightkoreanbbq.com/img/logo-8colorfnb.jpg' alt='8colorsfnb-logo' height='120px' style='margin-left: 200px;'><br></body></html>";
+  $MESSAGE_BODY .= "<img src='https://eightkoreanbbq.com/img/logo-8colorfnb.jpg' alt='8colorsfnb-logo' height='120px' style='margin:auto;'><br></body></html>";
 
   mail($ToEmail,  $EmailSubject, $MESSAGE_BODY, $mailheader, "-f$email");
 }
