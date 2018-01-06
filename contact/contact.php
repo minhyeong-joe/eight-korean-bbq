6ddce1a4e61ab8070640ef6afeab20086bc3ec75
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-01-05 19:30:35 -0800

    email php testing

--- contact/contact.php
+++ contact/contact.php
@@ -12,12 +12,12 @@ if (count($_POST)>0) {
   $mailheader .= "Reply-To: $email \r\n";
   $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
   $maiheader .="MIME-Version: 1.0\r\n";
-  $MESSAGE_BODY = "<html><head><link href='https://fonts.googleapis.com/css?family=Lato|Montserrat' rel='stylesheet'></head><body style='text-align:center;'><br><br><br><h2 style='font-family: Lato, sans-serif;'>You have a new submission from <a href='https://eightkoreanbbq.com/contact'>Eight Korean BBQ</a></h2><hr style='width:700px; margin: 20px 0;'><br>";
-  $MESSAGE_BODY .= "<table rules='all' style='border-color: #666; width: 650px; font-family: Montserrat, sans-serif;' cellpadding='10'>";
+  $MESSAGE_BODY = "<html><head><link href='https://fonts.googleapis.com/css?family=Lato|Montserrat' rel='stylesheet'></head><body><br><br><br><h2 style='font-family: Lato, sans-serif;'>You have a new submission from <a href='https://eightkoreanbbq.com/contact'>Eight Korean BBQ</a></h2><hr style='width:700px; margin: 20px 0;'><br>";
+  $MESSAGE_BODY .= "<table rules='all' style='border-color: #666; width: 650px; font-family: Montserrat, sans-serif; margin: auto;' cellpadding='10'>";
   $MESSAGE_BODY .= "<tr style='background: #eee;'><td style='width:120px; text-align:center;'><strong>Name:</strong></td><td>$name</td></tr>";
   $MESSAGE_BODY .= "<tr><td style='text-align:center;'><strong>Email:</strong></td><td><a href='mailto:$email'>$email</a></td></tr>";
   $MESSAGE_BODY .= "<tr><td style='text-align:center;'><strong>Message:</strong></td><td>$message</td></tr></table><br><br>";
-  $MESSAGE_BODY .= "<img src='https://eightkoreanbbq/img/logo-8colorfnb.jpg' alt='8-logo' height='120px' style='margin-left: 200px;'><br></body></html>";
+  $MESSAGE_BODY .= "<img src='https://eightkoreanbbq.com/img/logo-8colorfnb.jpg' alt='8-logo' height='120px' style='margin-left: 200px;'><br></body></html>";
 
   mail($ToEmail,  $EmailSubject, $MESSAGE_BODY, $mailheader, "-f$email");
 }
